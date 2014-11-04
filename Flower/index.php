2bc64e110edaa4add48e5c6116199af7b0422b0d
AlphaPL <ares_real@wp.pl>
2014-11-04 17:42:21 +0100

    Fixed site.

--- Flower/index.php
+++ Flower/index.php
@@ -1,17 +1,26 @@
 ﻿<!DOCTYPE html>
-
+<?php
+// Start the session
+session_start();
+?>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Zaloguj się!</title>
   <link rel="stylesheet" href="css/style.css">
-  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  
+  <script>
+	function validateForm()
+	{
+		return true;
+	}
+  </script>
 </head>
 <body>
   <section class="container">
     <div class="login">
       <h1>Zaloguj się</h1>
-      <form method="post" action="glowna_strona.php">
+      <form method="post" action="glowna_strona.php" onsubmit="return validateForm()">
         <p><input type="text" name="login" value="" placeholder="Nazwa użytkownika bądź email"></p>
         <p><input type="password" name="password" value="" placeholder="hasło"></p>
         <p class="remember_me">
