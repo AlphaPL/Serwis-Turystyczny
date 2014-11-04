7c433f4d01fea5a42ee146595dd7238ecae3a50a
Pawel <zekfir@gmail.com>
2014-11-04 18:18:05 +0100

    Dodawanie wpisu

---
+++ dodaj_wpis.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Flower</title>
+<meta http-equiv="Content-Language" content="English" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+<body>
+
+<div id="wrap">
+
+<div id="header">
+<h1>Portal wycieczek</h1>
+<h2>Pochwal sie swoimi cudownymi przeżyciami</h2>
+</div>
+
+<div id="right">
+<table class = "tabelka">
+  <tr>
+    <td>Miniaturka</td>
+    <td>Tytuł</td> 
+    <td>Autor</td>
+    <td>Ocena</td>
+  </tr>
+  <tr>
+    <td><img src="http://i2.cdn.turner.com/cnn/dam/assets/130618174740-01-syria-refugees-horizontal-gallery.jpg"></img></td>
+    <td>Syria, jaka piękna</td> 
+    <td>Jacek Jackowski</td>
+	<td></td>
+  </tr>
+</table>
+</div>
+
+<div id="left"> 
+<h3>Categories :</h3>
+<ul>
+<li><a href="#">Dodaj wycieczkę</a></li> 
+<li><a href="#">Lista wycieczek</a></li> 
+<li><a href="#">Powiadomienia</a></li> 
+</ul>
+
+</div>
+<div style="clear: both;"> </div>
+
+<div id="footer">
+</div>
+</div>
+
+</body>
+</html>
