9194a913c5af563a8af51f0531fbf188c24d1d6c
AlphaPL <ares_real@wp.pl>
2014-11-04 17:28:09 +0100

    Added stub code that fills table, that will be exchanged to database call.

--- Flower/glowna_strona.php
+++ Flower/glowna_strona.php
@@ -23,12 +23,18 @@
     <td>Autor</td>
     <td>Ocena</td>
   </tr>
-  <tr>
-    <td><img src="http://i2.cdn.turner.com/cnn/dam/assets/130618174740-01-syria-refugees-horizontal-gallery.jpg"></img></td>
-    <td>Syria, jaka piękna</td> 
-    <td>Jacek Jackowski</td>
-	<td></td>
-  </tr>
+ <?php
+ echo "<tr>";
+ $array = array(array("http://i2.cdn.turner.com/cnn/dam/assets/130618174740-01-syria-refugees-horizontal-gallery.jpg","Syria, jaka piękna","Jacek Jackowski",5.0));
+		foreach ($array as &$value) {
+		echo"<td><img src=\"". $value[0] ."\"></img></td>";
+		echo"<td>". $value[1] ."</td>";
+		echo"<td>". $value[2] ."</td>";
+		echo"<td>". $value[3] ."</td>";
+		}
+ echo "</tr>";
+ ?>
+
 </table>
 </div>
 
