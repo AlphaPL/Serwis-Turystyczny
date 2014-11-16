d5a9378a7fa4050248249ff105cef0e55e592a4d
AlphaPL <ares_real@wp.pl>
2014-11-16 14:06:43 +0100

    Added login.

--- Flower/validator.php
+++ Flower/validator.php
@@ -6,16 +6,42 @@ $login = $_POST["login"];
 
 $password = $_POST["password"];
 
+ class MyDB extends SQLite3
+ {
+    function __construct()
+    {
+       $this->open('test.db');
+    }
+ }
+ $db = new MyDB();
+ if(!$db){
+    echo $db->lastErrorMsg();
+ } else {
+    echo "Opened database successfully\n";
+ }
 
+ $sql =<<<EOF
+    SELECT * from USERS 
+    WHERE LOGIN LIKE '%login%';
+EOF;
+$sql = str_replace("%login%",$login,$sql);
+$sql = str_replace("%password%",$password,$sql);
+echo ($sql);
 
-if (empty($login) || empty($password)){
+ $ret = $db->query($sql);
+ $row = $ret->fetchArray(SQLITE3_ASSOC);
+ console.log($row);
+ $db->close();
+ if(!$row){
 	header("Location: index.php");
-}
-else
+ }
+ else
 {
 	$_SESSION["login"] = $login;
 	header("Location: glowna_strona.php");
 }
+ echo "Operation done successfully\n";
+ 
 
 }
 ?>
