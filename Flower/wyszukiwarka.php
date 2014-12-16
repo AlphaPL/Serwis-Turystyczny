61f53daf962e6395bacdd3790eab3db2bbaefd08
233241 <mprzystupa@wp.pl>
2014-12-16 19:01:31 +0100

    jakis tam tytul

--- Flower/wyszukiwarka.php
+++ Flower/wyszukiwarka.php
@@ -2,12 +2,20 @@
 
 function wyszukaj($title, $author, $grade_from, $grade_to, $difficulty_from, $difficulty_to, $budget_from, $budget_to, $country, $db)
 {
-	$sql = "SELECT * FROM TRIPS WHERE ID >= 0";
+	$sql = "SELECT * FROM TRIPS WHERE ID >= 0 AND AUTHOR='$author'";
 
+	// to oczywiscie dopiero sama funkcja bez gui i umieszczenia na stronce ale zamierzam niedlugo to zrobic
+	
+	// wywolanie 
+	// $ret=wyszukaj(0, "test", 0, 0, 0, 0, 0, 0, 0, $db);
+	
+	// na podstawie tego naprawic ponizsze if'y
+	// blad: zapomnialem o ''
+	
 	if ($title != '')
 		$sql .= " AND TITLE=%title%";
-	if ($author != '')
-		$sql .= " AND AUTHOR=%author%";
+	//if ($author != '')
+	//	$sql .= " AND AUTHOR=%author%";
 		
 	if ($grade_from != '')
 		$sql .= " AND GRADE>=%grade_from%";
